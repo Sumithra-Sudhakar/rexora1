2cf61da0c8965d28a622059e1945b44999619107
Sumithra-Sudhakar <sumisooriya@gmail.com>
2022-02-21 22:14:07 +0530

    signup page done

---
+++ lib/screens/signin.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:
+      Text("Sign In ")
+    );
+  }
+}
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rexora1/screens/signin.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -24,7 +25,8 @@ class SignUp extends StatelessWidget {
           children: [
             Expanded(
               flex: 1,
-                child: Image.asset("assets/sign_up.png"))
+                child: Image.asset("assets/sign_up.png", fit: BoxFit.fill
+                  ,))
 ,Expanded(
         flex:2,
                 child: ListView(
@@ -40,13 +42,86 @@ class SignUp extends StatelessWidget {
     ),
 Padding(padding: EdgeInsets.fromLTRB(20, 90, 20, 20),
 
-child: ElevatedButton(
+child: ElevatedButton.icon(
 
+icon: Icon(Icons.phone, size: 24,color: Color(0xff171B72),),
+  label: Text("Sign Up with phone", style: GoogleFonts.montserrat(
+    color: Color(0xff171B72),
+    fontSize: 16
+
+  ),),
   onPressed: (){},
-  child:
 
-  ),
-),)
+style: ElevatedButton.styleFrom(
+  elevation: 2,
+  padding: EdgeInsets.all( 10.0),
+  primary: Colors.white,
+  shape: new RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(20.0)
+  )
+),
+
+),),
+
+    Padding(padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+
+      child: ElevatedButton.icon(
+
+        icon: Icon(Icons.facebook, size: 24,color: Color(0xff171B72),),
+        label: Text("Sign Up with Facebook", style: GoogleFonts.montserrat(
+            color: Color(0xff171B72),
+            fontSize: 16
+
+        ),),
+        onPressed: (){},
+
+        style: ElevatedButton.styleFrom(
+            elevation: 2,
+            padding: EdgeInsets.all( 10.0),
+            primary: Colors.white,
+            shape: new RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)
+            )
+        ),
+
+      ),),
+
+    Padding(padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+
+      child: ElevatedButton.icon(
+
+        icon: Icon(Icons.mail_outline, size: 24,color: Color(0xff171B72),),
+        label: Text("Sign Up with Google", style: GoogleFonts.montserrat(
+            color: Color(0xff171B72),
+            fontSize: 16
+
+        ),),
+        onPressed: (){},
+
+        style: ElevatedButton.styleFrom(
+            elevation: 2,
+            padding: EdgeInsets.all( 10.0),
+            primary: Colors.white,
+            shape: new RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)
+            )
+        ),
+
+      ),),
+    Padding(
+      padding: EdgeInsets.all(20.0),
+      child: TextButton(
+        onPressed: (){
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const SignIn()));
+        },
+        child: Text("Already have an account? Sign in ", style: GoogleFonts.montserrat(
+          fontSize: 16,
+          color: Colors.white,
+
+        ),),
+      ),
+    )
     
 
   ],
