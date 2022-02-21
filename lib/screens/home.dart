5b825ad26dbfb6b01bd47a5bb521cd635edfd588
Sumithra-Sudhakar <sumisooriya@gmail.com>
2022-02-21 19:55:52 +0530

    signup page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rexora1/screens/signup.dart';
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -11,24 +12,18 @@ class Home extends StatelessWidget {
         alignment: Alignment.bottomCenter,
         constraints: BoxConstraints.expand(),
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/intro_bg.png"),
-            fit: BoxFit.cover
-          )
-        ),
+            image: DecorationImage(
+                image: AssetImage("assets/intro_bg.png"), fit: BoxFit.cover)),
         child: Padding(
           padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 30.0),
-          child: TextButton
-            (child: Text("The dream relationship you always wanted!", style:   GoogleFonts.montserrat(
-            color: Colors.white
-          )),
-            onPressed: (){
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const SignUp()));
+          child: TextButton(
+            child: Text("The dream relationship you always wanted!",
+                style: GoogleFonts.montserrat(color: Colors.white)),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignUp()));
             },
-            ),
+          ),
         ),
       ),
     );
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -6,8 +6,54 @@ class SignUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFF4A5BB6),
+              Color(0xFF18246D)
+            ]
+          )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+                child: Image.asset("assets/sign_up.png"))
+,Expanded(
+        flex:2,
+                child: ListView(
 
-    )
+  children: [
+    Align(
+      alignment: Alignment.topCenter,
+      child: Text("Sign Up",
+      style:  GoogleFonts.montserrat(color: Colors.white,
+        fontSize: 48,
+        fontWeight: FontWeight.w400
+          ),),
+    ),
+Padding(padding: EdgeInsets.fromLTRB(20, 90, 20, 20),
+
+child: ElevatedButton(
+
+  onPressed: (){},
+  child:
+
+  ),
+),)
+    
+
+  ],
+))
+          ],
+        ),
+      ),
+    );
   }
 }
